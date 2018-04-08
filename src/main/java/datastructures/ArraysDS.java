c52e526046962580a6ed7783cc9ff26e86e4ff2b
mzap <marco.m.zappala@gmail.com>
2018-04-08 23:37:01 +0200

    arrayds first version

---
+++ src/main/java/datastructures/ArraysDS.java
@@ -0,0 +1,56 @@
+package datastructures;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class ArraysDS {
+
+    static int[] reverseArray(int[] a) {
+        int lengthOfTheArray = a.length;
+        if (lengthOfTheArray == 0 || lengthOfTheArray == 1) {
+            return a;
+        }
+
+        int maxIndex = lengthOfTheArray - 1;
+        int ascendingIndex = 0;
+        int descendingIndex = maxIndex;
+
+        int swappingSupport = 0;
+        while (ascendingIndex < descendingIndex) {
+            swapElements(a, ascendingIndex, descendingIndex);
+            ascendingIndex++;
+            descendingIndex--;
+
+        }
+        return a;
+
+    }
+
+    private static void swapElements(int[] a, int ascendingIndex, int descendingIndex) {
+        int swappingSupport;
+        swappingSupport = a[ascendingIndex];
+        a[ascendingIndex] = a[descendingIndex];
+        a[descendingIndex] = swappingSupport;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static int[] execute(String[] args) throws IOException {
+
+        int[] arrayToReverse = getArrayToReverse(args);
+
+        int[] res = reverseArray(arrayToReverse);
+
+        return res;
+    }
+
+    public static int[] getArrayToReverse(String[] args) {
+        int[] arrayToReverse = new int[Integer.parseInt(args[0])];
+        String[] arrayOfStrings = args[1].split(" ");
+        for (int arrItr = 0; arrItr < arrayOfStrings.length; arrItr++) {
+            arrayToReverse[arrItr] = Integer.parseInt(arrayOfStrings[arrItr]);
+        }
+        return arrayToReverse;
+    }
+
+}
---
+++ src/test/java/ArraysDSTest.java
@@ -0,0 +1,69 @@
+import java.io.IOException;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import datastructures.ArraysDS;
+
+import static junit.framework.TestCase.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ArraysDSTest {
+
+    @Test
+    public void returnSameArrayIfLessThanTwoElements(){
+        String[] inputsForSingleElementArray = new String[2];
+        inputsForSingleElementArray[0] = "1";
+        inputsForSingleElementArray[1] = "3";
+
+        try {
+            int[] reversedArray = ArraysDS.execute(inputsForSingleElementArray);
+            assertTrue(reversedArray[0] == Integer.parseInt(inputsForSingleElementArray[1]));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    @Test
+    public void returnReversedArrayIfSizeHigherThanOneAndEven(){
+        String[] inputsForMultipleAndEvenelemnts = new String[2];
+        inputsForMultipleAndEvenelemnts[0] = "4";
+        inputsForMultipleAndEvenelemnts[1] = "1 2 3 4";
+
+        reverseAndVerify(inputsForMultipleAndEvenelemnts);
+
+    }
+
+    @Test
+    public void returnReversedArrayIfSizeHigherThanOneAndOdd(){
+        String[] inputsForMultpleAndOddElements = new String[2];
+        inputsForMultpleAndOddElements[0] = "5";
+        inputsForMultpleAndOddElements[1] = "1 2 3 4 5";
+
+        reverseAndVerify(inputsForMultpleAndOddElements);
+
+    }
+
+    private void reverseAndVerify(String[] inputsForMultpleAndOddElements) {
+        try {
+            int[] reversedArray = ArraysDS.execute(inputsForMultpleAndOddElements);
+            int[] arrayToReverse = ArraysDS.getArrayToReverse(inputsForMultpleAndOddElements);
+            int ascendingIndex = 0;
+            int descendingIndex = reversedArray.length - 1;
+
+            while(ascendingIndex < descendingIndex) {
+                assertEquals(reversedArray[ascendingIndex], arrayToReverse[descendingIndex]);
+                ascendingIndex++;
+                descendingIndex--;
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
